dc8a19a7024ba9b67c8ee3077aa4402ff9a272a9
Tongaston <gaston.moure@gmail.com>
2024-04-10 17:20:37 +0200

    second challenge added "Error message"

---
+++ Error message/index.js
@@ -0,0 +1,7 @@
+let errorParagraph = document.getElementById("error");
+
+
+function error() {
+errorParagraph.textContent = "Something went wrong, please try again later.";
+errorParagraph.style.color = "red";
+}
