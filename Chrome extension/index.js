dd56a68f10b248ab57fe3fbb7325b481cfce542a
Gaston Moure <gaston.moure@gmail.com>
2024-05-04 12:12:43 +0200

    functionality delete btn

---
+++ Chrome extension/index.js
@@ -0,0 +1,41 @@
+let myLeads = []
+const inputEl = document.getElementById('input-el')
+const buttonSave = document.getElementById('input-btn')
+const buttonDelete = document.getElementById('delete-btn')
+const ulEl = document.getElementById('ul-el')
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  render(myLeads)
+}
+
+function render(arr) {
+  let listItems = ''
+  for (let i = 0; i < arr.length; i++) {
+    listItems += `<li>
+      <a targuet="_blank" href="${arr[i]}">
+      ${arr[i]}
+      </a>
+      </li>`
+  }
+  ulEl.innerHTML = listItems
+}
+
+buttonSave.addEventListener('click', () => {
+  if (inputEl.value === '') {
+    return
+  } else {
+    myLeads.push(inputEl.value)
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    render(myLeads)
+    inputEl.value = ''
+  }
+})
+
+buttonDelete.addEventListener('dblclick', () => {
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
