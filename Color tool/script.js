d73bed2100f5a14e8564a1d176f1470b06ef748f
Gaston Moure <gaston.moure@gmail.com>
2024-06-20 15:59:04 +0200

    convert hexadecimal a rgb color

--- Color tool/script.js
+++ Color tool/script.js
@@ -12,7 +12,32 @@ const updateColor = () => {
   const inputHex = hexColorInput.value
   if (!isValidHex(inputHex)) return
 
-  currentColorBox.style.backgroundColor = inputHex
+  const strippedHex = inputHex.replace('#', '')
+  currentColorBox.style.backgroundColor = '#' + strippedHex
 }
 
-hexColorInput.addEventListener('input', updateColor)
+hexColorInput.addEventListener('keyup', updateColor)
+
+const convertHexToRGB = (hex) => {
+  if (!isValidHex(hex)) return
+
+  let strippedHex = hex.replace('#', '')
+  if (strippedHex.length === 3) {
+    strippedHex =
+      strippedHex[0] +
+      strippedHex[0] +
+      strippedHex[1] +
+      strippedHex[1] +
+      strippedHex[2] +
+      strippedHex[2]
+  } else if (strippedHex.length === 6) {
+    strippedHex = strippedHex
+  }
+
+  const r = parseInt(strippedHex.substring(0, 2), 16)
+  const g = parseInt(strippedHex.substring(2, 4), 16)
+  const b = parseInt(strippedHex.substring(4, 6), 16)
+
+  return { r, g, b }
+}
+console.log(convertHexToRGB('000'))
